5f418bb82a03f6d68621f5c6285a052a5cb51e9b
FRDRK <federicoaugelli@gmail.com>
2023-07-01 13:00:03 +0200

    database added

--- backend/bank/database/databank.py
+++ backend/bank/database/databank.py
@@ -12,24 +12,20 @@ database.commit()
 
 def register(name, username, password):
     try:
-        #data  = """INSERT INTO users
-        #                  (id, name, amount, username, password)
-        #                  VALUES (?, ?, ?, ?, ?);"""
-
-        test = """INSERT INTO users
-                          (id, name, amount, username, password) 
-                           VALUES 
-                          (1,'James',100,'fede','rico')"""
-
-        #data_tuple = (1, name, 100, username, password)
-        db.execute(test)
+        database = sqlite3.connect("bank.db")
+        db = database.cursor()
+        data  = """INSERT INTO users
+                          (name, amount, username, password)
+                          VALUES (?, ?, ?, ?);"""
+        data_tuple = (name, 100, username, password)
+        db.execute(data, data_tuple)
         database.commit()
         return("executed corr")
     except sqlite3.Error as error:
         return("Failed to insert data into sqlite table", error)
     finally:
-        #if database:
-        #    database.close()
+        if database:
+            database.close()
         print("The SQLite connection is closed")
 
 
