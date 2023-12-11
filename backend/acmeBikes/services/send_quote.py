b54e2f3172f79f1359df041a5b8503b57ba96ae7
Castroo99 <48692149+Castroo99@users.noreply.github.com>
2023-12-11 16:46:16 +0100

    send quote

--- backend/acmeBikes/services/send_quote.py
+++ backend/acmeBikes/services/send_quote.py
@@ -1,4 +1,31 @@
+import requests
+import pycamunda
+import pycamunda.processdef
+from pycamunda.message import CorrelateSingle
+from dotenv import load_dotenv
+import os
+
 def send_quote(process_instance_id, process_dict, discount):
-    """ chiamata AcmeDB per verificare customizzazione """
+    """ chiamata AcmeDB per mandare il preventivo """
     print(discount)
-    return {"quote": discount}
+    load_dotenv()
+    CAMUNDA_URL = os.getenv("CAMUNDA_URL")
+    print(f"send_quote {process_instance_id}")
+    try:
+        # list out keys and values separately
+        key_list = list(process_dict.keys())
+        val_list = list(process_dict.values())        
+        # print key with val 100
+        position = val_list.index(process_instance_id)
+        resale_process_instance_id = key_list[position]
+        
+        process_dict[process_instance_id] = resale_process_instance_id
+        
+        msg = CorrelateSingle(CAMUNDA_URL, message_name="order_accepted",
+                              process_instance_id=resale_process_instance_id
+                              )
+        msg()
+    except Exception as e:
+        print(e)
+        pass
+    return {"order_accepted": True}
